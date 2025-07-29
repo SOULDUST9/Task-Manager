4e8886f9d75782a99c714b78a4989808f8cde69a
Sebastian Janeczek <sjane089@uottawa.ca>
2025-07-28 22:30:18 -0400

    updated ui

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const pastWeeksList = document.getElementById("0-task-list");
 const thisWeekList = document.getElementById("1-task-list");
 const nextWeekList = document.getElementById("2-task-list");
 const upcomingList = document.getElementById("3-task-list");
@@ -8,6 +9,7 @@ function showTasks() {
     thisWeekList.innerHTML = "";
     nextWeekList.innerHTML = "";
     upcomingList.innerHTML = "";
+    pastWeeksList.innerHTML = "";
 
     const today = new Date();
     const endOfThisWeek = new Date(today);
@@ -25,20 +27,32 @@ function showTasks() {
         card.classList.add("task-card", "readonly");
 
         card.innerHTML = `
-            <p><strong>Task:</strong> ${task.task}</p>
             <p><strong>Category:</strong> ${task.category}</p>
+            <p><strong>Task:</strong> ${task.task}</p>
             <p><strong>Start Date:</strong> ${task.startDate}</p>
             <p><strong>Due Date:</strong> ${task.dueDate}</p>
             <button class="complete-btn" data-index="${index}">Complete</button>
         `;
 
-        if (due < endOfThisWeek) {
+        if (due < today) {
+            pastWeeksList.appendChild(card);
+        } 
+        else if (due <= endOfThisWeek) {
             thisWeekList.appendChild(card);
-        } else if (due < endOfNextWeek) {
+        } 
+        else if (due <= endOfNextWeek) {
             nextWeekList.appendChild(card);
-        } else {
+        } 
+        else {
             upcomingList.appendChild(card);
         }
+
+        const completebtn = card.querySelector(".complete-btn");
+        completebtn.addEventListener("click", function(){
+            tasks.splice(index, 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            showTasks();
+        });
     });
 }
 
